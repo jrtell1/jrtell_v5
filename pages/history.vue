7528ec09798e1ad0d1b1e310d66813080bb44dcd
John <john-robin@jrtell.se>
2019-08-13 13:33:48 +0200

    fixes

--- pages/history.vue
+++ pages/history.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="container mx-auto my-6 px-4">
     <div class="employment">
-      <h4>Alektum Group</h4>
-      <div>2019 - Current</div>
-      <div>Front-End Developer</div>
-      <div>
-        <a class="button w-48 my-0 py-2" href="https://www.alektumgroup.se/" target="_blank" rel="noopener noreferrer">Check it out</a>
+      <h4 class="flex-1">Alektum Group</h4>
+      <div class="flex-1">2019 - Current</div>
+      <div class="flex-1">Front-End Developer</div>
+      <div class="w-48">
+        <a class="button my-0 py-2" href="https://www.alektumgroup.se/" target="_blank" rel="noopener noreferrer">Check it out</a>
       </div>
     </div>
 
     <div class="employment">
-      <h4>Rebel Walls</h4>
-      <div>2016 - 2019</div>
-      <div>Systems Developer</div>
-      <div>
-        <a class="button w-48 my-0 py-2" href="https://rebelwalls.com/" target="_blank" rel="noopener noreferrer">Check it out</a>
+      <h4 class="flex-1">Rebel Walls</h4>
+      <div class="flex-1">2016 - 2019</div>
+      <div class="flex-1">Systems Developer</div>
+      <div class="w-48">
+        <a class="button my-0 py-2" href="https://rebelwalls.com/" target="_blank" rel="noopener noreferrer">Check it out</a>
       </div>
     </div>
 
     <div class="employment">
-      <h4>Self-employed</h4>
-      <div>2013 - 2016</div>
-      <div>Fullstack Developer</div>
-      <div></div>
+      <h4 class="flex-1">Self-employed</h4>
+      <div class="flex-1">2013 - 2016</div>
+      <div class="flex-1">Fullstack Developer</div>
+      <div class="w-48"></div>
     </div>
   </div>
 </template>
@@ -45,6 +45,10 @@
   @media screen and (max-width: 600px) {
     .employment {
       display: block;
+    }
+
+    .button {
+      @apply mt-6;
     }
   }
 
