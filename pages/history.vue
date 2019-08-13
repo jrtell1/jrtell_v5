300f743724b86b1df30d83c545c1e108a8ffcca6
John <john-robin@jrtell.se>
2019-08-13 13:32:02 +0200

    Added work history

--- pages/history.vue
+++ pages/history.vue
@@ -1,5 +1,30 @@
 <template>
-  
+  <div class="container mx-auto my-6 px-4">
+    <div class="employment">
+      <h4>Alektum Group</h4>
+      <div>2019 - Current</div>
+      <div>Front-End Developer</div>
+      <div>
+        <a class="button w-48 my-0 py-2" href="https://www.alektumgroup.se/" target="_blank" rel="noopener noreferrer">Check it out</a>
+      </div>
+    </div>
+
+    <div class="employment">
+      <h4>Rebel Walls</h4>
+      <div>2016 - 2019</div>
+      <div>Systems Developer</div>
+      <div>
+        <a class="button w-48 my-0 py-2" href="https://rebelwalls.com/" target="_blank" rel="noopener noreferrer">Check it out</a>
+      </div>
+    </div>
+
+    <div class="employment">
+      <h4>Self-employed</h4>
+      <div>2013 - 2016</div>
+      <div>Fullstack Developer</div>
+      <div></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +34,21 @@
 </script>
 
 <style scoped>
+  .employment {
+    display: flex;
+    justify-content: space-between;
+    transition: all 200ms ease-in-out;
 
+    @apply p-4 my-4 bg-gray-200;
+  }
+
+  @media screen and (max-width: 600px) {
+    .employment {
+      display: block;
+    }
+  }
+
+  .employment:hover {
+    @apply shadow-lg;
+  }
 </style>
