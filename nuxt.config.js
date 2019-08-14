e3f5e6d119dc0142832c8898828101b0a9bbbfa1
John <john-robin@jrtell.se>
2019-08-14 21:41:18 +0200

    description

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
       { property: 'og:title', content: 'John-Robin Tell' },
-      { property: 'og:description', content: 'Fullstack Developer with an JavaScript obsession...' },
+      { property: 'og:description', content: 'Fullstack Developer' },
       { property: 'og:image', content: 'https://res.cloudinary.com/jrtell/image/upload/w_200,h_200,c_fill,g_face,f_auto/v1552242769/jrtell/Snapchat-1254887129' }
     ],
     link: [
