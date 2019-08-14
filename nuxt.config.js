509fb70da22bee671f83c1aea0fd7200e9828023
John <john-robin@jrtell.se>
2019-08-14 21:39:14 +0200

    Added meta data to html head

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,18 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    title: 'John-Robin Tell',
     meta: [
       { charset: 'utf-8' },
+      { name: 'theme-color', content: '#2b6cb0' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { property: 'og:title', content: 'John-Robin Tell' },
+      { property: 'og:description', content: 'Fullstack Developer with an JavaScript obsession...' },
+      { property: 'og:image', content: 'https://res.cloudinary.com/jrtell/image/upload/w_200,h_200,c_fill,g_face,f_auto/v1552242769/jrtell/Snapchat-1254887129' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
